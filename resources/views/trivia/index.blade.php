f3ca2b92313c36d0f3321c60bbe1d6e2886fbf88
Ali <malim92@live.com>
2023-12-01 22:27:36 +0200

    setting up the project done

--- resources/views/trivia/index.blade.php
+++ resources/views/trivia/index.blade.php
@@ -11,17 +11,37 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div class="container">
-        <h1 class="mt-5">Trivia App</h1>
-        <p>Welcome to the Trivia App!</p>
+    <header class="navbar navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">Trivia App</a>
+        </div>
+    </header>
+    <x-alert class="alert alert-success"/>
+    <div class="container mt-4">
+        <h1 class="mb-4">Welcome to the Trivia App!</h1>
 
         <!-- Your Bootstrap-styled content goes here -->
+        <form>
+            <!-- Your form fields go here -->
+            <div class="mb-3">
+                <label for="fullName" class="form-label">Full Name</label>
+                <input type="text" class="form-control" id="fullName" name="fullName" required>
+            </div>
 
+            <!-- Other form fields... -->
+
+            <button type="submit" class="btn btn-primary">Start Trivia</button>
+        </form>
     </div>
+
+    <footer class="mt-5">
+        <div class="container text-center">
+            <p>&copy; 2023 Trivia App. All rights reserved.</p>
+        </div>
+    </footer>
 
     <!-- Bootstrap JS (optional, only if needed) -->
     <!-- Add the following lines before the closing body tag -->
     <!-- <script src="{{ asset('js/app.js') }}"></script> -->
-
 </body>
 </html>
